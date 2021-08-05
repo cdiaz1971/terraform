ebbdc4f8f80ad0a8a48a3baf844d07c1577828a4
Cesar Diaz <cesar@cesardiaz.me>
2021-08-05 23:46:42 +0000

    changed script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt install apache2 -y
-echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html
+echo '<h1><center>Super Awesome Test Website</center></h1>' > index.html
 sudo mv index.html /var/www/html
 sudo systemctl start apache2 && sudo systemctl enable apache2
